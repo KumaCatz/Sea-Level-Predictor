7738d1238b161a89cb37fb16faf9113d8144816f
KumaCatz K <tod.kagan@gmail.com>
2024-12-06 23:10:19 +0000

    feat: plt scatter

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -8,16 +8,23 @@ def draw_plot():
     print(df)
 
     # Create scatter plot
-
+    plt.scatter(
+        x=df['Year'],
+        y=df['CSIRO Adjusted Sea Level']
+        )
 
     # Create first line of best fit
-
+    first_line = liregress(
+        x=df
+    )
 
     # Create second line of best fit
-
+    second_line = liregress()
 
     # Add labels and title
-
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
